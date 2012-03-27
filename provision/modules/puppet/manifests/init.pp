7ff93488b4354f84e4ef1429f01354ae020ec86f
Aaron Bull Schaefer <aaron@elasticdog.com>
2012-03-26 19:21:54 -0700

    Initial clone of puppet-sandbox repository

---
+++ provision/modules/puppet/manifests/init.pp
@@ -0,0 +1,49 @@
+# == Class: puppet
+#
+# This class installs and manages the Puppet client daemon.
+#
+# === Parameters
+#
+# [*ensure*]
+#   What state the package should be in. Defaults to +latest+. Valid values are
+#   +present+ (also called +installed+), +absent+, +purged+, +held+, +latest+,
+#   or a specific version number.
+#
+# === Actions
+#
+# - Install Puppet client package
+# - Ensure puppet-agent daemon is running
+#
+# === Requires
+#
+# === Sample Usage
+#
+#   class { 'puppet': }
+#
+#   class { 'puppet':
+#     ensure => '2.6.8-0.5.el5',
+#   }
+#
+class puppet(
+  $ensure = $puppet::params::client_ensure
+) inherits puppet::params {
+
+  package { 'puppet':
+    ensure => $ensure,
+  }
+
+  # required to start client agent on ubuntu
+  exec { 'start_puppet':
+    command => '/bin/sed -i /etc/default/puppet -e "s/START=no/START=yes/"',
+    onlyif  => '/usr/bin/test -f /etc/default/puppet',
+    require => Package[ 'puppet' ],
+    before  => Service[ 'puppet' ],
+  }
+
+  service { 'puppet':
+    enable  => true,
+    ensure  => running,
+    require => Package[ 'puppet' ],
+  }
+
+}
